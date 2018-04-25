8255271f5677599e1cd717d80ffe5dbe91e9ec8d
Todd Smith <tsmith3171@gmail.com>
2018-04-25 16:07:12 -0400

    ngstyle example added and tested

--- src/app/samples/samples.component.ts
+++ src/app/samples/samples.component.ts
@@ -10,6 +10,27 @@ export class SamplesComponent implements OnInit {
   index = 0; // Typescript infers number type per linter.
   toggleUsFlag = true; // Typescript infers boolean type per linter.
   selectedUs: string;
+  color: string;
+
+  myStyle: {};
+
+  setStyle() {
+    this.myStyle = {
+      'font-weight': 'bold',
+      'font-size' : '24px',
+      'color' : this.generateRandomColor()
+    };
+  }
+
+  generateRandomColor() {
+    this.color = '#';
+    const letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    for (let i = 0; i < 6; i++) {
+      this.color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(this.color);
+    return this.color;
+  }
 
   selectUs(us) {
     this.selectedUs = us;
