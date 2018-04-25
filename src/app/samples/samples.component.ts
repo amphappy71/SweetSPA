7e02133e3e46e3def7b5d26a89cdb8179da78901
Todd Smith <tsmith3171@gmail.com>
2018-04-25 15:18:09 -0400

    added ngswitch example

--- src/app/samples/samples.component.ts
+++ src/app/samples/samples.component.ts
@@ -9,6 +9,11 @@ export class SamplesComponent implements OnInit {
   familyMemebers: string[] = ['Dad', 'Mom', 'Andrew', 'Todd II'];
   index = 0; // Typescript infers number type per linter.
   toggleUsFlag = true; // Typescript infers boolean type per linter.
+  selectedUs: string;
+
+  selectUs(us) {
+    this.selectedUs = us;
+  }
 
   toggleMember() {
     this.toggleUsFlag = !this.toggleUsFlag;
