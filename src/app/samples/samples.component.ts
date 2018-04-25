8b565468fbee4fd9bfeed2ce63fd4373044d0405
Todd Smith <tsmith3171@gmail.com>
2018-04-25 15:03:35 -0400

    added ngif example

--- src/app/samples/samples.component.ts
+++ src/app/samples/samples.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./samples.component.css']
 })
 export class SamplesComponent implements OnInit {
+  familyMemebers: string[] = ['Dad', 'Mom', 'Andrew', 'Todd II'];
+  index = 0; // Typescript infers number type per linter.
+  toggleUsFlag = true; // Typescript infers boolean type per linter.
+
+  toggleMember() {
+    this.toggleUsFlag = !this.toggleUsFlag;
+  }
 
   constructor() { }
 
