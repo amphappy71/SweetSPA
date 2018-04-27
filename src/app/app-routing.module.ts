5d7e80529347392e6d42e75ab1f2155422f14321
Todd Smith <tsmith3171@gmail.com>
2018-04-27 14:22:03 -0400

    changed icon, base nav testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { SamplesComponent } from './samples/samples.component';
 import { SweetContactComponent } from './sweet-contact/sweet-contact.component';
 import { SweetBlogComponent } from './sweet-blog/sweet-blog.component';
+import { SweetVisionComponent } from './sweet-vision/sweet-vision.component';
+import { AppComponent } from './app.component';
+import { SweetNavComponent } from './sweet-nav/sweet-nav.component';
+
 
 
 const appRoutes: Routes = [
-  {path: '', component: SamplesComponent},
+  {path: '', component: SweetVisionComponent},
   {path: 'contact', component: SweetContactComponent},
-  {path: 'blog', component: SweetBlogComponent}
+  {path: 'blog', component: SweetBlogComponent},
+  {path: 'samples', component: SamplesComponent}
 ];
 
 @NgModule({
--- src/app/sweet-nav/sweet-nav.component.ts
+++ src/app/sweet-nav/sweet-nav.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-sweet-nav',
   templateUrl: './sweet-nav.component.html',
   styleUrls: ['./sweet-nav.component.css']
 })
+
+
 export class SweetNavComponent implements OnInit {
 
   constructor() { }
